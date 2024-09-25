5b2fbf5624af4822b86ff7b49403b3650b5214d5
aktyz <zyta.slowianska@gmail.com>
2024-09-25 16:25:40 +0200

    feat: Start on the project and general understanding of the assignment

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-I want to check if the commit will appear as mine in GitHub
+# General idea
+
+I want to implement the stacs as lists of integers.
+Right now I think I will need:
+- structure for stack as a list
+- functions to sort the stack
+- functions to modify the order of items on the list
+- function to iterate throught a list
+
+=> best is to really crete the functions for Libft BONUS and then re-use them here with the stack or/and stack item structure.
+
+=> once Libft enriched for list functions I will use the Turk algorithm to sort the stacks
+
+=> I will also need to translate each operation on the list into the operations names given in the assignment
---
+++ main.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zslowian <zslowian@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/25 15:36:05 by zslowian          #+#    #+#             */
+/*   Updated: 2024/09/25 16:15:43 by zslowian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void    sort_input(char *input);
+
+int main(int argc, char *argv[])
+{
+    if (!argc || argc < 2)
+        return (0);
+    else
+        sort_input(argv[1]);
+    return (0);
+}
+
+void    sort_input(char *input)
+{
+        
+}
